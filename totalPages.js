1f825eff3fa9fd54e54520be88a3ae37f2c35b79
dodoza1415 <surachet.pich@kmutt.ac.th>
2022-09-10 20:33:50 +0700

    Update : Final Version / Completely test on fil test.js

--- totalPages.js
+++ totalPages.js
@@ -2,5 +2,18 @@ const { template } = require('@babel/core')
 
 function totalPages(arrayItems, rowsPerPage) {
   //code here
+  if (arrayItems == null || arrayItems == undefined){
+    return undefined
+  }
+  else if (rowsPerPage == null || rowsPerPage == undefined || rowsPerPage == 0){
+    return 1
+  }else{
+    return Math.ceil((arrayItems.length / rowsPerPage))
+  }
 }
+let array = [1,2,3,4,5,6,7,8,9,10]
+let array2 = [1,2,3,4,5]
+let arrayItems 
+let rowsPerPage = 20
+console.log(totalPages(arrayItems, rowsPerPage))
 module.exports = totalPages
